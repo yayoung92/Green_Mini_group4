6b8571f9eca794703808860d4e0b913b6b0d46a2
yayoung92 <yangga0744@gmail.com>
2024-01-25 15:33:47 +0900

    24.01.25 회원가입 아이디 중복 체크

--- greenMinigroup4/src/greenMinigroup4/Insert.java
+++ greenMinigroup4/src/greenMinigroup4/Insert.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import db.DBConnection;
 
 public class Insert {
+	
 	public void insert(Person person) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
--- greenMinigroup4/src/greenMinigroup4/Login.java
+++ greenMinigroup4/src/greenMinigroup4/Login.java
@@ -8,7 +8,7 @@ import db.DBConnection;
 
 public class Login {
 	User user = new User();
-	
+
 	// 로그인 기능
 	public int login(String name, String pw) {
 		Connection conn = null;
@@ -24,8 +24,9 @@ public class Login {
 			rs = pstmt.executeQuery();
 
 			if (rs.next()) {
-	
-			} else {
+				
+
+			}else {
 				System.out.println("회원정보가 없습니다.");
 				return 0;
 			}
@@ -35,45 +36,67 @@ public class Login {
 		}
 		return 1;
 	}
-	
+
 	// 회원가입 기능
 	public void join(String name, String pw) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
+		boolean result = checkID(name);
 
-		try {
-			conn = DBConnection.getConnection();
-			String sql = "insert into user (`u_id`,`u_password`) " + "values (?,?)";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, name);
-			pstmt.setString(2, pw);
-			pstmt.executeUpdate();
-
-		} catch (Exception e) {
-			e.printStackTrace();
+		if (!result) {
+			try {
+				conn = DBConnection.getConnection();
+				String sql = "insert into user (`u_id`,`u_password`) " + "values (?,?)";
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setString(1, name);
+				pstmt.setString(2, pw);
+				pstmt.executeUpdate();
+				
+				System.out.println();
+				System.out.println("==   성공! 회원가입 되었습니다.   ==");
+				System.out.println();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println();
+			System.err.println("아이디 중복입니다. 회원가입에 실패했습니다.");
+			System.out.println();
 		}
+//		finally{
+//			if(conn != null) {
+//				try{
+//					conn.close();
+//				} catch(Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		}
 	}
-	
+
 	// 아이디 중복 확인 기능
-	public boolean findID(String name) {
+	public boolean checkID(String name) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		
+
 		try {
 			conn = DBConnection.getConnection();
 			String sql = "select * from user where u_id=?";
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, name);
 			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
+
+			if (rs.next()) {
 				return true;
 			}
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return false;
 	}
+	
+	// 비밀번호 찾아서 비교해야하나? 비밀번호 틀리면 비밀번호 틀렸다고 알려주고 싶은뎅
+
 
 }
