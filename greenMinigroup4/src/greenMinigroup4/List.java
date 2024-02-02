dc3a1589eaff179337a22dd290e65ffbd3a5089c
jjj200820@gmail.com <jjj200820@gmail.com>
2024-02-02 17:47:37 +0900

    24-02-02 오후 5시 47분 커밋

--- greenMinigroup4/src/greenMinigroup4/List.java
+++ greenMinigroup4/src/greenMinigroup4/List.java
@@ -8,34 +8,30 @@ import java.sql.ResultSet;
 import db.DBConnection;
 
 public class List {
-	
-	public void listall() {
-		Connection con=null;
-		PreparedStatement pstmt=null;
-		
+
+	public void listall(int u_idx) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
 		try {
 			con = DBConnection.getConnection();
-			pstmt=con.prepareStatement("SELECT * FROM person ");
-		
-		ResultSet result=pstmt.executeQuery();
-		System.out.println(" 이름 |  나이  |  성별  |       번호       | MBTI |  주소  |  별명  |  소속 ");
-		while(result.next()) {
-			System.out.println(result.getString("name") + "\t" + result.getString("age")+"\t"+result.getString("gender")+
-					"\t"+result.getString("phoneNumber")+"\t"+result.getString("MBTI")+"\t"+result.getString("address")
-					+"\t"+result.getString("nickName")+"\t"+result.getString("category"));
-			System.out.println();
-			
-		
-		}
-		
-		
-		}
-		catch(Exception e) {
+			pstmt = con.prepareStatement("SELECT * FROM person where u_idx=?");
+			pstmt.setInt(1, u_idx);
+			ResultSet result = pstmt.executeQuery();
+
+			System.out.println(" 이름 |  나이  |  성별  |       번호       | MBTI |  주소  |  별명  |  소속 ");
+			while (result.next()) {
+				System.out.println(result.getString("name") + "\t" + result.getString("age") + "\t"
+						+ result.getString("gender") + "\t" + result.getString("phoneNumber") + "\t"
+						+ result.getString("MBTI") + "\t" + result.getString("address") + "\t"
+						+ result.getString("nickName") + "\t" + result.getString("category"));
+				System.out.println();
+
+			}
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	
-		
+
 }
