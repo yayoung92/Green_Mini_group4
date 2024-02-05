9a66d735c278095b17810a57ef456bd1f64cae0d
yayoung92 <yangga0744@gmail.com>
2024-02-05 12:56:16 +0900

    24.02.05 패키지 정리

--- greenMinigroup4/src/greenMinigroup4/Login.java
+++ greenMinigroup4/src/greenMinigroup4/Login.java
@@ -37,7 +37,7 @@ public class Login {
 		return 1;
 	}
 
-	// 회원가입 정보 받는 코드
+	// 로그인 정보 받는 코드
 	public User inputUser() {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("          <      로그인 하기      >          ");
@@ -48,57 +48,5 @@ public class Login {
 		String password = scan.next();
 
 		return new User(id, password);
-	}
-
-	// 회원가입 기능
-	public void join() {
-		User user = this.inputUser();
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		boolean result = checkID(user.getUserId());
-		Scanner scan = new Scanner(System.in);
-
-		if (!result) {
-			try {
-				conn = DBConnection.getConnection();
-				String sql = "insert into user (`u_id`,`u_password`) " + "values (?,?)";
-				pstmt = conn.prepareStatement(sql);
-				pstmt.setString(1, user.getUserId());
-				pstmt.setString(2, user.getUserPassword());
-				pstmt.executeUpdate();
-
-				System.out.println();
-				System.out.println("==   성공! 회원가입 되었습니다.   ==");
-				System.out.println();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		} else {
-			System.out.println();
-			System.err.println("아이디 중복입니다. 회원가입에 실패했습니다.");
-			System.out.println();
-		}
-	}
-
-	// 아이디 중복 확인 기능
-	public boolean checkID(String name) {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-
-		try {
-			conn = DBConnection.getConnection();
-			String sql = "select * from user where u_id=?";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, name);
-			rs = pstmt.executeQuery();
-
-			if (rs.next()) {
-				return true;
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return false;
 	}
 }
