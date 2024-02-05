de68686a06544babab79ef76fab89f47a9ef00a5
topingkkukkasoboro <qhtjd9822@naver.com>
2024-02-05 18:22:15 +0900

    0205

--- greenMinigroup4/src/db/DBConnection.java
+++ greenMinigroup4/src/db/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
 	public static Connection getConnection() throws SQLException, ClassNotFoundException {
 		Connection conn = null;
 
-		String url = "jdbc:mysql://192.168.0.38/ygy";
+		String url = "jdbc:mysql://192.168.0.38/kbs";
 		String id = "root";
 		String pw = "1234";
 		
