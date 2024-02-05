7de609d6f8ef8cccc580dc09350c3d2414fcb6ed
yayoung92 <140130695+yayoung92@users.noreply.github.com>
2024-02-05 17:55:56 +0900

    Update Main.java

--- greenMinigroup4/src/controller/Main.java
+++ greenMinigroup4/src/controller/Main.java
@@ -187,10 +187,4 @@ public class Main {
 		}
 		System.out.println(" =========  프로그램이 종료되었습니다.  ========= ");
 	}
-
-	private static void searchDetail(String name, int u_idx) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
